105d01f01aac993c44f9fcfb27344811d3e1e8ca
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2023-10-18 19:41:53 -0300

    configuracoes basicas do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
