690cd57034562736048b544af562bf33828df296
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2024-01-11 19:45:33 -0300

    config enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     baseUrl: 'http://localhost:8080/',
-    baseApiUrl: 'http://localhost:8080/api/'
+    baseApiUrl: 'http://localhost:4200/api/'
 
 };
